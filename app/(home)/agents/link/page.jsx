ee2d0d0c900a7fd4e572e6fca9075b1c82d1c385
roy <bmpss1026@gmail.com>
2024-07-10 04:44:12 -0700

    feat:新增link/page.jsx

--- app/(home)/agents/link/page.jsx
+++ app/(home)/agents/link/page.jsx
@@ -1,10 +1,9 @@
-"use client";
 import React from "react";
 
 const agents = [
-  { name: "James LLC", date: "2024-05-10 21:17:54" },
-  { name: "James LLC", date: "2024-05-10 21:17:54" },
-  { name: "James LLC", date: "2024-05-10 21:17:54" },
+  { name: "Inc", date: "2024-05-10 21:17:54" },
+  { name: "Inc", date: "2024-05-10 21:17:54" },
+  { name: "Inc", date: "2024-05-10 21:17:54" },
 ];
 
 const AgentsPage = () => {
@@ -13,34 +12,33 @@ const AgentsPage = () => {
       <div className="max-w-6xl w-full mt-[-100px]">
         <div className="flex justify-between items-center mb-6">
           <div className="flex flex-col">
-            <h1 className="text-3xl font-semibold mb-3">Agents</h1>
-            <a
-              href="/projects"
-              className="text-slate-700 underline hover:text-blue-900 transition duration-300 ease-in-out"
-              onClick={(e) => {
-                Link.href = "/agents/link";
-              }}
-            >
-              Agent Dashboard
-            </a>
+            <div className="flex">
+              <a
+                href="/agents"
+                className="text-slate-500 hover:text-blue-900 duration-300 ease-in-out"
+              >
+                Agents
+              </a>
+              <p className="ml-3">/</p>
+              <p className="ml-3">James LLC</p>
+            </div>
+
+            <h1 className="text-3xl font-semibold">Link Applications</h1>
           </div>
-          <button className="bg-green-700 text-white px-4 py-2 rounded-xl hover:bg-green-900">
-            Create New Agent
-          </button>
         </div>
         <div>
           {agents.map((agent, index) => (
             <div
               key={index}
-              className="bg-white p-4 rounded shadow mb-4 flex justify-between items-center border border-slate-300 w-full"
+              className="bg-white p-4 rounded shadow mb-4 flex justify-between items-center border border-slate-300"
             >
-              <div className="flex flex-col">
+              <div>
                 <h2 className="text-xl font-extrabold text-blue-950 mb-2">
                   {agent.name}
                 </h2>
                 <p className="text-gray-700">{agent.date}</p>
               </div>
-              <div className="flex flex-col space-y-2">
+              <div className="flex space-x-2">
                 <button className="bg-slate-50 text-black text-sm font-bold px-2 py-1 rounded-md border border-slate-500 hover:bg-slate-300">
                   Detail
                 </button>
